c1d9e2a510f912a3680a0819a5d4efff843c5d20
Tyler D <tyler@tdaigle.com>
2022-06-25 05:47:59 -0400

    showing and hiding of popups working

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ function App() {
   const video = "videos/nosferatu.mp4";
   const popupData = [
     {
-      time: 15,
+      time: 5,
       duration: 5,
       text: "This is a popup for 5 seconds",
       thumbnail: null,
     },
     {
-      time: 35,
+      time: 15,
       duration: 10,
       text: "This is a popup for 10 seconds",
       thumbnail: null,
--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,8 +1,39 @@
+import { useState } from "react";
 import styles from "../styles/VideoPlayer.module.css";
+
 export default function VideoPlayer({ videoSource, popupData }) {
+  const [popupVisible, setPopupVisible] = useState(false);
+  const [currentPopupIndex, setCurrentPopupIndex] = useState(0);
+  const [popupContent, setPopupContent] = useState("This is some test content");
+
+  const checkForPopupTime = (currentTime) => {
+    if (popupData && currentPopupIndex < popupData.length) {
+      console.log(`checking ${Math.floor(currentTime)}`);
+      if (popupData[currentPopupIndex].time === Math.floor(currentTime)) {
+        setPopupVisible(true);
+        console.log("Ok");
+        setTimeout(
+          () => setPopupVisible(false),
+          popupData[currentPopupIndex].duration * 1000
+        );
+        setCurrentPopupIndex(currentPopupIndex + 1);
+      }
+    }
+  };
   return (
     <div className={styles.videoPlayerContainer}>
-      <video className={styles.videoPlayer} controls>
+      {popupVisible && (
+        <div className={styles.popup}>
+          <p>{popupContent}</p>
+        </div>
+      )}
+
+      <video
+        onPlay={() => console.log("Play has started.")}
+        onTimeUpdate={(e) => checkForPopupTime(e.target.currentTime)}
+        className={styles.videoPlayer}
+        controls
+      >
         <source src={videoSource} type="video/mp4" />
       </video>
     </div>
